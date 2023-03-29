52a4ed401b41c4d2577920e1a3483fdec97661e1
Balajis007 <donbalajis007@gmail.com>
2023-03-29 14:31:28 +0530

    feat: some feature add my project

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,86 +1,136 @@
+//    register starts
 
-
-// Register form
 const form = document.getElementById("register");
+
 const f_name = document.getElementById("first_name");
+
 const l_name = document.getElementById("last_name");
+
 const mobilenumber = document.getElementById("mobile_number");
+
 const regemail = document.getElementById("regemail");
+
 const regpassword = document.getElementById("regpassword");
 
-const registerFormValidation = () => {
+
+const registerFromValidation = () => {       // this is a register page star and this store the value in localstorage 
+
     const f_name_value = f_name.value.trim();
     const l_name_value = l_name.value.trim();
     const mobile_value = mobilenumber.value.trim();
     const reg_email_value = regemail.value.trim();
     const reg_pass_value = regpassword.value.trim();
 
+    // empty array
     let record = [];
+
+
+
+    // empty object
     let user = {};
+
+
 
     let users = JSON.parse(localStorage.getItem('users')) ?? record;
 
-    user.first_name = f_name_value;
-    user.last_name = l_name_value;
-    user.mobile_number = mobile_value;
-    user.email = reg_email_value;
-    user.password = reg_pass_value;
-    user.id = users.length;
 
-    const isDuplicate = users.find(
-        (user) => user.email === reg_email_value || user.mobile_number === mobile_value
-    );
+    user["first_name"] = f_name_value,
+        user["last_name"] = l_name_value,
+        user["mobile_number"] = mobile_value,
+        user["email"] = reg_email_value,
+        user["password"] = reg_pass_value;
+        user["id"]=users.length;
 
-    if (isDuplicate) {
-        console.error("You already have an account");
-    } else {
-        users.push(user);
-        localStorage.setItem("users", JSON.stringify(users));
-        console.log("Account created successfully");
-    }
+
+
+        const isDuplicate = users.find(
+            (user) => user.email === reg_email_value || user.mobile_number === mobile_value
+        );
+
+        if (isDuplicate) {
+            Notify.error("You already have an account");
+        } else {
+            users.push(user);
+            localStorage.setItem("users", JSON.stringify(users));
+            Notify.success("Account created successfully");
+        }
+
+
 }
+
+
+
 
 form.addEventListener("submit", e => {
+
     e.preventDefault();
-    registerFormValidation();
+
+    registerFromValidation();
 });
 
 
-// Login form
+
+//    login starts
+
 const checkform = document.getElementById("checkregister");
+
 const username = document.getElementById("username");
+
 const password = document.getElementById("password");
 
-const checkRegisterFormValidation = () => {
-    try {
-        const user_data = JSON.parse(localStorage.getItem("users"));
-        const username_value = username.value.trim();
-        const password_value = password.value.trim();
 
-        if (user_data === null) {
-            console.error("You must sign up before logging in");
-        } else {
-            const email_check = user_data.filter(obj => obj.email === username_value && obj.password === password_value);
 
-            if (email_check.length === 1) {
-                let profile_email = username_value;
-                localStorage.setItem("profile_email", profile_email);
-                window.location.href = "pages/home/home.html";
-                console.log("Successfully logged in");
-                checkform.reset();
-            } else {
-                console.error("Email or password incorrect");
-            }
+const checkregisterFromValidation = () => {
+
+
+    const user_data = JSON.parse(localStorage.getItem("users"));
+
+    const username_value = username.value.trim();
+    const password_value = password.value.trim();
+
+    if (user_data === null) {
+        Notify.error("you first sinup next you come and try")
+
+    } else {
+
+        const email_check = user_data.filter(obj => obj.email === username_value && obj.password === password_value)
+
+        if (email_check.length === 1) {
+
+            let profile_email = username_value;
+
+            console.log(profile_email);
+
+            localStorage.setItem("profile_email", profile_email);
+
+            window.location.href = "pages/home/home.html"
+
+            Notify.success("Successfully Login");
+
+            checkform.reset();
+
+
+
+
         }
-    } catch (error) {
-        console.error(error);
+        else {
+            Notify.error("Email or Password Incorrect");
+        }
+
+
     }
+
 }
 
-checkform.addEventListener("submit", e => {
+
+checkform.addEventListener("submit", (e) => {
     e.preventDefault();
-    checkRegisterFormValidation();
+    checkregisterFromValidation();
 });
+
+
+
+
 
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
